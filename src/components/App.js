ef0a8697bfa1485c7550f7f4c777b02c6ebe3067
shu Lin <bbqlinshu@gmail.com>
2022-01-25 17:04:43 -0500

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,49 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toyList, setToyList] = useState([]);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  useEffect(()=>{
+    fetch("http://localhost:3001/toys")
+    .then(r=>r.json())
+    .then(data=>setToyList(data))
+  }, [])
+
+  function handleAddToy(newToy){
+    const newToyList = [...toyList, newToy]
+    setToyList(newToyList)
+  }
+
+  function handleDeleteToy(deleteToy){
+    const newToyList = toyList.filter((toy)=>{
+      return toy.id!==deleteToy.id
+    })
+    setToyList(newToyList)
+  }
+
+  function handleAddLikes(addLikeToy){
+    const newToyList = toyList.map((toy)=>{
+      if(toy.id===addLikeToy.id){
+        return addLikeToy
+      }else{
+        return toy
+      }
+    })
+    setToyList(newToyList)
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+      {showForm ? <ToyForm onAddToy={handleAddToy}/> : null}
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toyList={toyList} onDeleteToy={handleDeleteToy} onAddLikes={handleAddLikes}/>
     </>
   );
 }
